531047502beb0d780784b48dce2e7dc0b6cb0362
Gaurav <157672208+GauravMaan@users.noreply.github.com>
2024-08-27 19:36:21 +0530

    1528. Shuffle String

---
+++ 1528. Shuffle String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char [] shuffled=new char[s.length()];
+        for(int i=0;i<s.length();i++){
+           shuffled[indices[i]]=s.charAt(i);
+        }
+        //System.gc();
+        return new String(shuffled);
+    }
+}
