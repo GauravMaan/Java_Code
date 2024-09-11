2db4aa6e7e2b28cf9d108373e6a1910a0e9b0e6c
Gaurav <157672208+GauravMaan@users.noreply.github.com>
2024-09-11 13:23:42 +0530

    58. Length of Last Word

---
+++ 58. Length of Last Word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        s = s.trim();
+        String[] words = s.split(" ");
+        
+        if (words.length == 0) {
+            return 0;
+        }
+        String lastWord = words[words.length - 1];
+        return lastWord.length();
+    }
+}
